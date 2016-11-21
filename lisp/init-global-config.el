5dd0c39014cf301c9701322f0c016f6fffe3012e
tzg157 <tzg157@163.com>
2016-11-21 10:04:52 +0800

    emacs 配置

---
+++ lisp/init-global-config.el
@@ -0,0 +1,7 @@
+;; init-global-config
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-linum-mode t)
+
+(provide 'init-global-config)
---
+++ lisp/init-slime.el
@@ -0,0 +1,14 @@
+;;(setq inferior-lisp-program "/usr/bin/sbcl")
+;;(add-to-list 'load-path "~/.emacs.d/elpa/slime/")
+;;(require 'slime)
+;;(slime-setup)
+
+(dolist (p '("~/.emacs.d/lisp/" "~/.emacs.d/elpa/slime-20161102.711")) (push p load-path))
+
+(setf inferior-lisp-program "/usr/bin/sbcl")
+
+(setq slime-contrib '(slime-fancy))
+
+(require 'slime)
+
+(provide 'init-slime)
